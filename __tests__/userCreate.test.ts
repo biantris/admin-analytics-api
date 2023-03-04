d13c79ad180024a0ec6ea3d1c7f87c680f64364b
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2023-03-04 19:17:10 -0300

    test(user): add test to user (#3)

---
+++ __tests__/userCreate.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest";
+import prisma from "../prisma/prisma.config";
+import { app } from "../supertest.config";
+
+describe("POST /users", () => {
+  afterAll(async () => {
+    await prisma.user.deleteMany({});
+  });
+
+  it("should create a user", async () => {
+    const response = await request(app).post("/users").send({
+      name: "John Doe",
+      email: "john@gmail.com",
+      password: "12345678",
+      access_level: "EMPLOYEE",
+    });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("name", "John Doe");
+  });
+});
