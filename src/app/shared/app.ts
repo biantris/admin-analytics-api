6ae0df5488a656201472a4a36e1758ae196eb6f8
biantris <anabeatrizxoliveira@gmail.com>
2023-03-10 19:38:00 -0300

    fix cors

--- src/app/shared/app.ts
+++ src/app/shared/app.ts
@@ -9,9 +9,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// origin: "https://admin-analytics.vercel.app/",
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: 'http://localhost:3000/',
+  })
+);
 
 app.get('/open', (req, res) => {
   res.status(200).json({
